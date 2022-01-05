277da97dac5686fda8fe93e64008642c0fde6e71
Stephen Rice <stephenrice1@gmail.com>
2022-01-05 21:13:07 +0000

    add start script

--- app.js
+++ app.js
@@ -164,6 +164,7 @@ app.use((err, req, res, next) => {
     res.send('oh boy, something went wrong!')
 })
 
-app.listen(3000, () => {
-    console.log('Serving on port 3000 - Chur!')
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`Serving on port ${port} - Chur!`)
 })
